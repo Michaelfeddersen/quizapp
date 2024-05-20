e2b3825f63e56763de0300c84623dd9dd99f716d
Michael Feddersen <165910787+Michaelfeddersen@users.noreply.github.com>
2024-05-20 22:01:07 +0200

    allquestion

--- main.js
+++ main.js
@@ -15,70 +15,70 @@ let questions = [
         "answer_4": "Es Trenc",
         "correct_answer": "4"
     },
-    // {
-    //     "question": "Welches ist das bekannteste Fest auf Mallorca?",
-    //     "answer_1": "Sant Antoni",
-    //     "answer_2": "Sant Joan",
-    //     "answer_3": "Fira de la Llum",
-    //     "answer_4": "Fiesta de San Fermin",
-    //     "correct_answer": "2"
-    // },
-    // {
-    //     "question": "Welches ist das Wahrzeichen von Palma de Mallorca?",
-    //     "answer_1": "Kathedrale La Seu",
-    //     "answer_2": "Castell de Bellver",
-    //     "answer_3": "Platja de Palma",
-    //     "answer_4": "Palma Aquarium",
-    //     "correct_answer": "1"
-    // },
-    // {
-    //     "question": "Welche ist die beliebteste Touristenattraktion in Mallorca?",
-    //     "answer_1": "Cuevas del Drach",
-    //     "answer_2": "Puig Major",
-    //     "answer_3": "Alcudia Altstadt",
-    //     "answer_4": "Torrent de Pareis",
-    //     "correct_answer": "1"
-    // },
-    // {
-    //     "question": "Welcher berühmte Künstler lebte und arbeitete lange Zeit auf Mallorca?",
-    //     "answer_1": "Salvador Dalí",
-    //     "answer_2": "Pablo Picasso",
-    //     "answer_3": "Joan Miró",
-    //     "answer_4": "Antoni Gaudí",
-    //     "correct_answer": "3"
-    // },
-    // {
-    //     "question": "Welche ist die höchste Erhebung auf Mallorca?",
-    //     "answer_1": "Puig Major",
-    //     "answer_2": "Serra de Tramuntana",
-    //     "answer_3": "Puig de Massanella",
-    //     "answer_4": "Cap de Formentor",
-    //     "correct_answer": "1"
-    // },
-    // {
-    //     "question": "Welche ist die wichtigste Einnahmequelle für Mallorca?",
-    //     "answer_1": "Tourismus",
-    //     "answer_2": "Landwirtschaft",
-    //     "answer_3": "Fischerei",
-    //     "answer_4": "Bauindustrie",
-    //     "correct_answer": "1"
-    // },
-    // {
-    //     "question": "Welche ist die größte Bucht von Mallorca?",
-    //     "answer_1": "Bucht von Palma",
-    //     "answer_2": "Bucht von Alcudia",
-    //     "answer_3": "Bucht von Pollença",
-    //     "answer_4": "Bucht von Cala Figuera",
-    //     "correct_answer": "2"
-    // },
-    // {
-    //     "question": "Welche ist die typische Spezialität der mallorquinischen Küche?",
-    //     "answer_1": "Paella",
-    //     "answer_2": "Gazpacho",
-    //     "answer_3": "Frit Mallorquí",
-    //     "answer_4": "Tortilla Española",
-    //     "correct_answer": "4"
-    // }
+    {
+        "question": "Welches ist das bekannteste Fest auf Mallorca?",
+        "answer_1": "Sant Antoni",
+        "answer_2": "Sant Joan",
+        "answer_3": "Fira de la Llum",
+        "answer_4": "Fiesta de San Fermin",
+        "correct_answer": "2"
+    },
+    {
+        "question": "Welches ist das Wahrzeichen von Palma de Mallorca?",
+        "answer_1": "Kathedrale La Seu",
+        "answer_2": "Castell de Bellver",
+        "answer_3": "Platja de Palma",
+        "answer_4": "Palma Aquarium",
+        "correct_answer": "1"
+    },
+    {
+        "question": "Welche ist die beliebteste Touristenattraktion in Mallorca?",
+        "answer_1": "Cuevas del Drach",
+        "answer_2": "Puig Major",
+        "answer_3": "Alcudia Altstadt",
+        "answer_4": "Torrent de Pareis",
+        "correct_answer": "1"
+    },
+    {
+        "question": "Welcher berühmte Künstler lebte und arbeitete lange Zeit auf Mallorca?",
+        "answer_1": "Salvador Dalí",
+        "answer_2": "Pablo Picasso",
+        "answer_3": "Joan Miró",
+        "answer_4": "Antoni Gaudí",
+        "correct_answer": "3"
+    },
+    {
+        "question": "Welche ist die höchste Erhebung auf Mallorca?",
+        "answer_1": "Puig Major",
+        "answer_2": "Serra de Tramuntana",
+        "answer_3": "Puig de Massanella",
+        "answer_4": "Cap de Formentor",
+        "correct_answer": "1"
+    },
+    {
+        "question": "Welche ist die wichtigste Einnahmequelle für Mallorca?",
+        "answer_1": "Tourismus",
+        "answer_2": "Landwirtschaft",
+        "answer_3": "Fischerei",
+        "answer_4": "Bauindustrie",
+        "correct_answer": "1"
+    },
+    {
+        "question": "Welche ist die größte Bucht von Mallorca?",
+        "answer_1": "Bucht von Palma",
+        "answer_2": "Bucht von Alcudia",
+        "answer_3": "Bucht von Pollença",
+        "answer_4": "Bucht von Cala Figuera",
+        "correct_answer": "2"
+    },
+    {
+        "question": "Welche ist die typische Spezialität der mallorquinischen Küche?",
+        "answer_1": "Paella",
+        "answer_2": "Gazpacho",
+        "answer_3": "Frit Mallorquí",
+        "answer_4": "Tortilla Española",
+        "correct_answer": "4"
+    }
 ];
 
 let currentQuestion = 0;
